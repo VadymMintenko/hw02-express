eb83ee2c9798e2ba6741586ce098f6d4eb780727
VadymMintenko <vad.m1ntenko@gmail.com>
2023-06-10 16:44:15 +0300

    hw-3

---
+++ controllers/contacts-controllers.js
@@ -0,0 +1,64 @@
+const contactsService = require("../models/contacts");
+const { HttpError } = require("../helpers");
+const { ctrlWrapper } = require("../decorator");
+const Contact = require("../models/contact");
+
+const getAllContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404, `Contact with ${contactId} not found`);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw HttpError(404, `Contact with ${contactId} not found`);
+  }
+  res.status(204).send();
+};
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, `Contact with ${contactId} not found`);
+  }
+  res.json(result);
+};
+
+const updateFavoriteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, `Contact with ${contactId} not found`);
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateFavoriteContact: ctrlWrapper(updateFavoriteContact),
+};
---
+++ decorator/index.js
@@ -0,0 +1,9 @@
+const ctrlWrapper = require("./ctrlWrapper");
+const validateBody = require("./validateBody");
+const isValidId = require("./itsValidId");
+
+module.exports = {
+  ctrlWrapper,
+  validateBody,
+  isValidId,
+};
